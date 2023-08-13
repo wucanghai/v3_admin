ef5ec00a17166b6871b573067f9a351d75b57b0f
wucanghai <970710823@qq.com>
2023-08-13 15:21:15 +0800

    fix: elementplus router pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+
+import store from '@/store'
+import router from '@/router'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(store).use(router)
+
+app.mount('#app')
